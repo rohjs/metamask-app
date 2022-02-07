7b0a3537728261dd3ee567f45069148079733316
Roh Woohyeon <roh.woohyeon@gmail.com>
2022-02-07 21:30:04 +0900

    🚧 Connect to ethereum accounts

--- src/Main.tsx
+++ src/Main.tsx
@@ -1,9 +1,18 @@
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import './App.css'
 
 function Main() {
+  const [address, setAddress] = useState('')
   const navigate = useNavigate()
+
+  const connect = async () => {
+    const accounts = await window.ethereum.request({
+      method: 'eth_requestAccounts',
+    })
+
+    if (accounts) setAddress(accounts[0])
+  }
 
   useEffect(() => {
     if (!window.ethereum) {
@@ -11,7 +20,18 @@ function Main() {
     }
   }, [navigate])
 
-  return <div className="Main">Main</div>
+  return (
+    <div className="Main">
+      Main
+      <dl>
+        <dt>Address</dt>
+        <dd>{address || '-'}</dd>
+      </dl>
+      <button onClick={connect} type="button">
+        Connect
+      </button>
+    </div>
+  )
 }
 
 export default Main
