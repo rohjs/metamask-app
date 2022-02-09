273fa1929ecadcc991292532db8d6dcf12f7607f
Kimbug <kimbug@planetariumhq.com>
2022-02-09 13:56:17 +0900

    📦 Install redux

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
+
+import { store } from './store'
+
 import App from './App'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-)
+const Root = () => {
+  return (
+    <React.StrictMode>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </React.StrictMode>
+  )
+}
+
+ReactDOM.render(<Root />, document.getElementById('root'))
